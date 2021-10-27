8e5ce4c8e89cb92da641562204504baf33fe65cc
Valerian21 <72431080+Valerian21@users.noreply.github.com>
2021-10-27 12:51:36 +0900

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Valutka
+{
+    class Main
+    {
+        string API_KEY = "1d2965cd4ccee6e70a1b";
+        public decimal DeserialDataCurrency(string code)
+        {
+            var url = $"https://free.currconv.com/api/v7/convert?q={code}&compact=ultra&apiKey={API_KEY}";
+            var webClient = new WebClient();
+            var jsonData = string.Empty;
+            decimal answer = decimal.Zero;
+            try
+            {
+                jsonData = webClient.DownloadString(url);
+                JObject currencyData = JObject.Parse(jsonData);
+                answer = Convert.ToDecimal(currencyData[code]);
+            }
+            catch (Exception error)
+            {
+                System.Windows.MessageBox.Show(error.Message);
+            }
+            return answer;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Valutka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertCurrency_Click(object sender, RoutedEventArgs e)
+        {
+            Main main = new Main();
+            string code = $"{cbCurrencyBegin.Text}_{cbCurrencyEnd.Text}";
+            decimal resultConvert = Convert.ToDecimal(tbCurrencyBegin.Text) * main.DeserialDataCurrency(code);
+            lblFinalRates.Content = $"{resultConvert}";
+        }
+    }
+}
